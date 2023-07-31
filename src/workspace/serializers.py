e90fd05901b0ea96a3f82dd52bc42470ebb48413
Pamilerin Idowu <pamilerinid@gmail.com>
2023-07-31 15:31:08 +0100

    update tools and subs return

--- src/workspace/serializers.py
+++ src/workspace/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import Lesson
 
 class LessonSerializer(serializers.ModelSerializer):
+    tools = serializers.StringRelatedField(many=True)
+    substances = serializers.StringRelatedField(many=True)
     class Meta:
         model = Lesson
         fields = '__all__'
