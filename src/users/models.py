71ae61ec9cc4a5e976db4b5fb0f588ed34f7e1fb
Pamilerin Idowu <pamilerinid@gmail.com>
2023-08-25 06:40:26 +0100

    update

--- src/users/models.py
+++ src/users/models.py
@@ -115,6 +115,6 @@ class User(AbstractUser):
         }
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return f'{self.first_name} {self.last_name}'
 
 saved_file.connect(generate_aliases_global)
