89574c9a623b02a16ef12f92e60f5d0731450ee0
Pamilerin Idowu <pamilerinid@gmail.com>
2024-02-11 09:26:22 +0100

    update req file

--- src/workbench/views.py
+++ src/workbench/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+from chempy import balance_stoichiometry
+
 from .models import *
 from .serializers import *
 
