bf770b30586754a6bca12d8e7d01adaae8bbd129
JhimmieC137 <toluwalope.david@gmail.com>
2024-05-09 00:31:34 +0100

    error handling and filter query for search

--- src/config/common.py
+++ src/config/common.py
@@ -293,9 +293,10 @@ THUMBNAIL_ALIASES = {
 
 # Django Rest Framework
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.OrderingFilter'],
-    'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 18)),
+    'DEFAULT_PAGINATION_CLASS': 'src.common.pagination.CustomPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
     'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S.%fZ',
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
--- src/workspace/views.py
+++ src/workspace/views.py
@@ -43,9 +43,68 @@ class LessonViewSet(ModelViewSet):
     """
     queryset  = Lesson.objects.all().order_by('title')
     serializer_class =  LessonSerializer
+    filterset_fields = ['title']
 
     def get_queryset(self):                                      
         return super().get_queryset()
+    
+    def perform_create(self, serializer):
+        serializer.save()
+        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response({"message": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({"message": "Lesson created successfully", "data": serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def list(self, request):
+        filter_obj = request.query_params.get('title')
+        queryset = self.get_queryset()
+        if filter_obj:
+            queryset = self.queryset.filter(name__contains = filter_obj)
+        try:
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response({"message": "Lessons retrived successfully", "data": serializer.data}, status=status.HTTP_200_OK)
+        except:
+            return Response({"message": "Something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return Response({"message":"Lesson retrived successfully", "data": serializer.data}, status=status.HTTP_200_OK)
+        except:
+            return Response({"message": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    def perform_update(self, serializer):
+        serializer.save()
+        
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        try:
+            instance = self.get_object()
+        except:
+            return Response({"message": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response({"message": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
+        self.perform_update(serializer)
+        return Response({"message": "Lesson updated successfully", "data": serializer.data}, status=status.HTTP_200_OK)        
+        
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
     
 
 # class ReactionListCreateView(generics.ListCreateAPIView):
