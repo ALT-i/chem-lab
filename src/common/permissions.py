637e5605d1e1688efbc8e5b127ae6646f013ca59
JhimmieC137 <toluwalope.david@gmail.com>
2024-05-28 03:21:42 +0100

    fix email case sensitivity

--- src/common/permissions.py
+++ src/common/permissions.py
@@ -4,13 +4,4 @@ from src.users.models import User
 class IsActiveUser(permissions.BasePermission):
         
     def has_permission(self, request, view):
-        users = User.objects.all()
-        for user in users:
-            print(user.email)
-            if User.objects.get(email = user.email.lower()):
-                pass
-            else:
-                user.email = user.email.lower()
-                user.save()
-            print(user.email)
         return not request.user.is_active
